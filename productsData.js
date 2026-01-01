67b29220b7e8a3dd0e52f453b574d06b2a53278d
erictranfe34 <erictranfe34@gmail.com>
2026-01-01 19:43:07 +0100

    Add files via upload

---
+++ productsData.js
@@ -0,0 +1,38 @@
+const products = [
+    {
+        id: 1,
+        title: "75,000+ Viral Reels Bundle, TikTok, Instagram & YouTube",
+        image: "https://i.etsystatic.com/62529683/r/il/84a50e/7337809412/il_794xN.7337809412_c3ux.jpg",
+        description: "💼 What’s Inside 🎞️ 75,000+ Viral Reels – High-quality, faceless, aesthetic reels optimized for Instagram, TikTok, YouTube Shorts & more. 🔓 PLR + MRR Included – Rebrand, resell, or bundle for 100% profit—no restrictions.",
+        originalPrice: 15.99,
+        salePrice: 4.99,
+        purchaseLink: "https://maken8n.gumroad.com/l/adventure-time-cat-pattern?wanted=true"
+    },
+    {
+        id: 2,
+        title: "100,000+ Viral Reels Bundle PLR MRR, AI Animated Content, Viral reels, TikTok, Instagram, YouTube",
+        image: "https://i.etsystatic.com/62529683/r/il/974811/7385682363/il_794xN.7385682363_ns0r.jpg",
+        description: "💼 What’s Inside 🎞️ 100,000+ Viral Reels – High-quality, faceless, aesthetic reels optimized for Instagram, TikTok, YouTube Shorts & more. 🔓 PLR + MRR Included – Rebrand, resell, or bundle for 100% profit—no restrictions.",
+        originalPrice: 14.99,
+        salePrice: 4.99,
+        purchaseLink: "https://maken8n.gumroad.com/l/alpino-doll-pattern?wanted=true"
+    },
+    {
+        id: 3,
+        title: "1450+ Motivational Reels Minimalist Video Content MRR PLR TikTok, Instagram, YouTube reels Digital Bundle",
+        image: "https://i.etsystatic.com/62529683/r/il/ff398b/7347443813/il_794xN.7347443813_2ct9.jpg",
+        description: "💼 What’s Inside 🎞️ 1450+ Viral Reels – High-quality, faceless, aesthetic reels optimized for Instagram, TikTok, YouTube Shorts & more. 🔓 PLR + MRR Included – Rebrand, resell, or bundle for 100% profit—no restrictions.",
+        originalPrice: 14.99,
+        salePrice: 4.99,
+        purchaseLink: "https://maken8n.gumroad.com/l/baby-giraffe-pattern?wanted=true"
+    },
+	{
+        id: 4,
+        title: "300 AI Baby Podcast Reels | Viral TikTok, Instagram, YouTube",
+        image: "https://i.etsystatic.com/62529683/r/il/7177a3/7493709201/il_794xN.7493709201_4c2p.jpg",
+        description: "💼 What’s Inside 🎞️ 300 Viral Reels – High-quality, faceless, aesthetic reels optimized for Instagram, TikTok, YouTube Shorts & more. 🔓 PLR + MRR Included – Rebrand, resell, or bundle for 100% profit—no restrictions.",
+        originalPrice: 14.99,
+        salePrice: 4.99,
+        purchaseLink: "https://maken8n.gumroad.com/l/baby-giraffe-pattern?wanted=true"
+    }
+];
